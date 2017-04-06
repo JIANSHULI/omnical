2279dd86a6635eb79018efea6ee7dfb0076897ff
Zaki Ali <zakiali88@gmail.com>
2017-04-06 13:38:40 -0700

    add order_data and modify pack_calpar/unpack_calpar in calib.RedundantInfo().
    This order_data() order's the data to omnical's internal format.
    However, it divides out by the intitial gains saved in the RedundantInfo
    object in the pack_calpar function.
    
    unpack calpar multiplies  the initial gains back into the solved for
    gains (from pack_calpar).
    
    changed instances of info to self.

--- src/calib.py
+++ src/calib.py
@@ -38,56 +38,79 @@ class RedundantInfo(info.RedundantInfo):
         """
         return 3 + 2*(nant+nubl) + bool(has_chi2ant)*nant
 
+    def order_data(self, data):
+        '''Create a data array ordered for use in _omnical.redcal. 'data' is
+        a dict whose keys are (i,j) antenna tuples; antennas i,j should be ordered to reflect
+        the conjugation convention of the provided data.  'dd' values are 2D arrays
+        of (time,freq) data.''' # XXX does time/freq ordering matter.  should data be 2D instead?
+        try: 
+            self._gains:
+        except(AttributeError):
+            raise RuntimeError("Call self.pack_calpar() first")
+
+        dd = [] 
+        for ai, aj in self.bl_order():
+            if data.has_key((ai, aj)):
+                dd.append( dd[ai, aj] / (np.conj(self._gains[ai]) * self._gains[aj]) )
+            else:
+                dd.append( np.conj(data[aj, ai]) / (np.conj(self._gains[aj]) * self._gains[ai] )  # are the gains conjugated correctly ???XXX
+        return np.array(dd.transpose((1,2,0))
+
+        
+
     def pack_calpar(self, calpar, gains=None, vis=None)
         '''Pack gain solutions and/or model visibilities for baseline types into a 'calpar' array that follows
         the internal data order used by omnical.  This function facilitates wrapping _omnical.redcal to
         abstract away internal data ordering when providing initial guesses for antenna gains and
         visibilities for ubls.
-        info: a RedundantInfo object
+        As of version 5: this function saves the gains as self._gains to be used in order data and unpack_calpar.
+        self: a RedundantInfo object
         calpar: the appropriately (time,freq,3+2*(nant+nubl)+nant) shaped array into which gains/vis will be copied
         gains: dictionary of antenna number: gain solution (vs time,freq)
         vis: dictionary of baseline: model visibility (vs time,freq).  baseline is cross-indexed to the appropriate
         baseline, so only one representative of each ubl type should be provided.'''
-        assert(calpar.shape[-1] == calpar_size(info.nAntenna, len(info.ublcount)))
-        nant = info.nAntenna
+        assert(calpar.shape[-1] == calpar_size(self.nAntenna, len(self.ublcount)))
+        nant = self.nAntenna
         if gains is not None:
-        self.gains = gains
-            for i,ai in enumerate(info.subsetant):
+            for i,ai in enumerate(self.subsetant):
                 if not gains.has_key(ai): continue
                 amp = np.log10(np.abs(gains[ai])); amp.shape = ((1,) + amp.shape)[-2:]
                 # XXX does phs need to be conjugated b/c omnical & aipy don't have same conj convention?
                 phs = np.angle(gains[ai]); phs.shape = ((1,) + phs.shape)[-2:]
                 calpar[...,3+i], calpar[...,3+nant+i] = amp, phs
+            self._gains = gains
         if vis is not None:
             for (ai,aj),v in vis.iteritems():
-                i,j = info.ant_index(ai), info.ant_index(aj)
-                n = info.bl1dmatrix[i,j] # index of this bl in info.bl2d
-                u = info.bltoubl[n] # index of ubl that this bl corresponds to
-                if tuple(info.bl2d[n]) != (i,j): # check if bl reversed wrt ubl
-                    assert(tuple(info.bl2d[n]) == (j,i))
+                i,j = self.ant_index(ai), self.ant_index(aj)
+                n = self.bl1dmatrix[i,j] # index of this bl in self.bl2d
+                u = self.bltoubl[n] # index of ubl that this bl corresponds to
+                if tuple(self.bl2d[n]) != (i,j): # check if bl reversed wrt ubl
+                    assert(tuple(self.bl2d[n]) == (j,i))
                     v = v.conj()
                 # XXX possible that frombuffer might do this a bit more efficiently
                 calpar[...,3+2*nant+2*u] = v.real # interleave real/imag
                 calpar[...,3+2*nant+2*u+1] = v.imag
         return calpar
 
-    def unpack_calpar(self, info, calpar):
+    def unpack_calpar(self, calpar):
         '''Parse the calpar result from omnical (complementary to pack_calpar).
         Result is parsed into meta, gains, and vis dicts which are returned.  meta has keys 'iter' and
         'chisq', gains as keys of antenna number, and vis has unique baseline solutions, indexed by a
         representative baseline.'''
         meta, gains, vis = {}, {}, {}
         meta['iter'],meta['chisq'] = calpar[...,0], calpar[...,2]
-        chisq_per_ant = calpar[...,calpar_size(info.nAntenna, len(info.ublcount), False):]
-        for i,ai in enumerate(info.subsetant):
-            gains[ai] = 10**calpar[...,3+i] * np.exp(1j*calpar[...,3+info.nAntenna+i])
+        chisq_per_ant = calpar[...,calpar_size(self.nAntenna, len(self.ublcount), False):]
+        for i,ai in enumerate(self.subsetant):
+            gains[ai] = 10**calpar[...,3+i] * np.exp(1j*calpar[...,3+self.nAntenna+i])
+            gains[ai] *= self._gains[ai] # multiply back in the initial gains.
             meta['chisq%d' % (ai)] = chisq_per_ant[...,i]
-        for u in xrange(len(info.ublcount)):
+        for u in xrange(len(self.ublcount)):
             # XXX possible that frombuffer might do this a bit more efficiently
-            v = calpar[...,3+2*info.nAntenna+2*u] + 1j*calpar[...,3+2*info.nAntenna+2*u+1]
-            n = info.ublindex[np.sum(info.ublcount[:u])]
-            i,j = info.bl2d[n]
-            vis[(info.subsetant[i],info.subsetant[j])] = v
+            v = calpar[...,3+2*self.nAntenna+2*u] + 1j*calpar[...,3+2*self.nAntenna+2*u+1]
+            n = self.ublindex[np.sum(self.ublcount[:u])]
+            i,j = self.bl2d[n]
+            vis[(self.subsetant[i],self.subsetant[j])] = v
+        del(self._gains)
         return meta, gains, vis
 
 # XXX maybe omnical should only solve one time at a time, so that prev sol can be used as starting point for next time
